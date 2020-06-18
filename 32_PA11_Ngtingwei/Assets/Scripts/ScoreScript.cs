d511965f67532bdb4c9bc6ebc196583545223f9c
Xenousss <ttng001@gmail.com>
2020-06-18 21:00:33 +0800

    Updated by Ng Ting Wei
    Updated by Ng Ting Wei

---
+++ 32_PA11_Ngtingwei/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int score;
+
+    public Text txtscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtscore.text = "Score: " + score;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            score += 1;
+        }
+    }
+}
